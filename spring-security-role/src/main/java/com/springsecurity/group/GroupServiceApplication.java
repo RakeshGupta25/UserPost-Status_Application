4c42992bc1607fff7f9f80138b6a8621ae32bff4
RakeshGupta25 <104621746+RakeshGupta25@users.noreply.github.com>
2022-07-15 18:30:03 +0530

    commit

---
+++ spring-security-role/src/main/java/com/springsecurity/group/GroupServiceApplication.java
@@ -0,0 +1,18 @@
+package com.springsecurity.group;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupServiceApplication.class, args);
+		System.out.println("hello");
+		
+		
+	}
+
+}
+
+
